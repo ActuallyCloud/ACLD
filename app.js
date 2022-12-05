f5a8a61d95fe80deff60e67a481fdaf3eb9527e1
Cloud <33647317+ActuallyCloud@users.noreply.github.com>
2022-12-04 16:08:09 -0800

    Initial commit
    Initial files and setup for express, node.js, and handlebars.

---
+++ app.js
@@ -0,0 +1,29 @@
+var express=require('express');
+var exphbs=require('express-handlebars');
+var path=require('path');
+
+var app=express();
+
+// Set View Folder
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up View engine
+app.engine('handlebars', exphbs.create({
+    defaultLayout: 'main',
+    layoutsDir: app.get('views') + '/layouts'
+}).engine);
+app.set('view engine', 'handlebars');
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ToDo App Start
+app.get('/',function(req,res){
+    res.render('index');
+});
+// ToDo App End
+
+// Run the Server
+app.listen('3000',function(){
+    console.log('Server is running at PORT '+3000);
+});
