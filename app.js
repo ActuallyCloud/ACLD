d6c1567ff41c4d02805454bbed42e33be9056822
Cloud <33647317+ActuallyCloud@users.noreply.github.com>
2022-12-11 10:41:58 -0800

    Basic changes and added multiple pages

--- app.js
+++ app.js
@@ -4,26 +4,35 @@ var path=require('path');
 
 var app=express();
 
-// Set View Folder
+// Initial setup
 app.set('views', path.join(__dirname, 'views'));
 
-// Set up View engine
 app.engine('handlebars', exphbs.create({
     defaultLayout: 'main',
     layoutsDir: app.get('views') + '/layouts'
 }).engine);
 app.set('view engine', 'handlebars');
 
-// Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ToDo App Start
+// Webpages and server responses
 app.get('/',function(req,res){
     res.render('index');
 });
-// ToDo App End
 
-// Run the Server
+app.get('/contact',function(req,res){
+    res.render('contact');
+});
+
+app.get('/wellness',function(req,res){
+    res.render('wellness');
+});
+
+app.get('/privacy',function(req,res){
+    res.render('privacy');
+});
+
+// On startup, display this.
 app.listen('3000',function(){
-    console.log('Server is running at PORT '+3000);
+    console.log('Website is online at port ' + 3000);
 });
