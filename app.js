bc315362a1dd1762b1ecac85d8fa232759cbdda2
Cloud <33647317+ActuallyCloud@users.noreply.github.com>
2022-12-11 11:37:54 -0800

    Streamlined some changes
    Removed the server redirect page, made all contact links changeable by global variable, and updated with new better icons.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Webpages and server responses
+// Webpages.
 app.get('/',function(req,res){
     res.render('index');
 });
@@ -24,12 +24,29 @@ app.get('/contact',function(req,res){
     res.render('contact');
 });
 
+app.get('/server',function(req,res){
+    res.render('server');
+});
+
 app.get('/wellness',function(req,res){
     res.render('wellness');
 });
 
 app.get('/privacy',function(req,res){
     res.render('privacy');
+});
+
+// External links. These can be changed here and update globally.
+app.get('/ext/github', function(req,res){
+    res.redirect('https://github.com/ActuallyCloud');
+});
+
+app.get('/ext/discord', function(req,res){
+    res.redirect('https://discord.com/invite/JQu5k4W6pT');
+});
+
+app.get('/ext/email', function(req,res){
+    res.redirect('mailto:dev@acld.me');
 });
 
 // On startup, display this.
