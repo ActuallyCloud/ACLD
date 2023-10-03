76b9b1a08c363cf4e35fbff316db4a6acf4e3b76
Cloud <33647317+ActuallyCloud@users.noreply.github.com>
2023-10-03 13:26:23 -0700

    Added Linker prototype link

--- app.js
+++ app.js
@@ -65,6 +65,10 @@ app.get('/ext/telepathyadd', function(req,res){ // Adds Telepathy (unreleased bo
     res.redirect('https://discord.com/invite/');
 });
 
+app.get('/ext/figma-linker', function (req, res) { // Redirects to the Figma prototype for Linker, a new type of messaging app.
+    res.redirect('https://www.figma.com/proto/Hg8flxttRtx0qVc2obWXg9/Linker?type=design&node-id=1-2&t=qDSNgGGG3OLeuw37-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2&mode=design');
+});
+
 // Uptime monitoring response here. Also error 404 if no page found.
 app.get('/up',function(req,res){
     res.status(200).send('Up! o.0');
